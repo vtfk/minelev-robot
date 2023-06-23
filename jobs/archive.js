b225d2df90783272136bd1d4137397b3e42ec375
jorgtho <jorgen.thorsnes@gmail.com>
2023-06-23 13:15:24 +0200

    added job archive

---
+++ jobs/archive.js
@@ -0,0 +1,50 @@
+
+const axios = require('axios').default
+const { archive } = require('../config')
+const { logger } = require('@vtfk/logger')
+const hasSvarutException = require('../lib/has-svarut-exception')
+
+const repackArchiveParents = (parents) => {
+  const result = []
+  for (const parent of parents) {
+    result.push({
+      ssn: parent.privatePerson.ssn,
+      role: 'Mottaker',
+      isUnofficial: true
+    })
+  }
+  return result
+}
+
+module.exports = async (jobDef, documentData) => {
+  const mapper = jobDef.mapper
+  if (!mapper) {
+    throw new Error('No mapper or default mapper is defined in options. Please provide a custom mapper or default mapper in flow definition')
+  }
+  logger('info', ['archive', 'Mapper is defined in options. Will use it.'])
+
+  const archiveData = mapper(documentData)
+
+  if (documentData.flowStatus.addParentsIfUnder18 ) { // If addParentsIfUnder18 is there, it means it is enabled
+    if (hasSvarutException(documentData.flowStatus.syncElevmappe.result.privatePerson.ssn)) {
+      logger('info', ['archive', 'student has svarut exception, will not add parents'])
+    } else {
+      logger('info', ['archive', 'job "addParentsIfUnder18" is enabled. Will add parents if we have them'])
+      archiveData.contacts = repackArchiveParents(documentData.flowStatus.addParentsIfUnder18.result)
+    }
+  }
+
+  const payload = {
+    system: 'minelev',
+    template: `${documentData.type}-${documentData.variant}`,
+    secure: jobDef.options?.secure || false,
+    parameter: archiveData
+  }
+
+  const headers = {
+    'Ocp-Apim-Subscription-Key': archive.ARCHIVE_SUBSCRIPTION_KEY
+  }
+  const { data } = await axios.post(`${archive.ARCHIVE_URL}/archive`, payload, { headers })
+  logger('info', ['archive', 'Successfully archived document', data.DocumentNumber])
+  return data
+}
