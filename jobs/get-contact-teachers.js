d49c4c9fb318621c382f03d4049532c2f2ac93a2
jorgtho <jorgen.thorsnes@gmail.com>
2023-06-23 13:16:11 +0200

    added job get contact teachers

---
+++ jobs/get-contact-teachers.js
@@ -0,0 +1,15 @@
+const { pifu} = require('../config')
+const generateJwt = require('../lib/generate-jwt')
+const axios = require('axios').default
+const { logger } = require('@vtfk/logger')
+
+module.exports = async (jobDef, documentData) => {
+  const token = generateJwt(pifu.PIFU_JWT_SECRET)
+  const studentUserName = documentData.student.username
+  if (!studentUserName) throw new Error('Must have "documentData.student.username" to be able to run job')
+  const url = `${pifu.PIFU_URL}/students/${studentUserName}/contactteachers`
+  logger('info', ['getContactTeachers', 'Fetching contact teachers from PIFU'])
+  const { data } = await axios.get(url, { headers: { Authorization: token } })
+  logger('info', ['getContactTeachers', `Got ${data.length} contact teachers from PIFU`])
+  return data
+}
