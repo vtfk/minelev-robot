48be8b50b54f5537c0d043b0e107959eda8d4e14
jorgtho <jorgen.thorsnes@gmail.com>
2023-06-28 14:22:28 +0200

    added main entry point

--- index.js
+++ index.js
@@ -3,10 +3,27 @@
   // Remember to close mongoclient when script is done
 
   const robot = require('./robot/index')
+  const { logger, logConfig } = require('@vtfk/logger')
+  const { getReadyDocuments } = require('./robot/get-ready-documents')
+  const { disconnect } = require('./lib/mongo-client')
+
+  logger('info', ['new run'])
+
+  try {
+    await getReadyDocuments()
+  } catch (error) {
+    logger('error', ['Error when fetching documents from mongodb', error.stack || error.toString()])
+  }
 
   try {
     await robot()
   } catch (error) {
-    console.log(error)
+    logger('error', ['Error when running robot', error.stack || error.toString()])
   }
+
+  logConfig({
+    prefix: false
+  })
+  await disconnect()
+  await logger('info', ['finished run'])
 })()
